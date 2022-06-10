91643165111c30d7eb5a2c0f70dc2f33d07a8ccb
Pravin Shinde <43694148+pravinshinde16@users.noreply.github.com>
2022-06-10 14:20:24 +0530

    Update login2.js

--- js/login2.js
+++ js/login2.js
@@ -2,7 +2,7 @@ function validate()
 {
     var username=document.getElementById("userid").value;
     var password=document.getElementById("pswrd").value;
-    if(username=="Mayur" && password=="Mayur@3101")
+    if(username=="" && password=="")
     {
         window.open('css/admin.html');
         return false;
